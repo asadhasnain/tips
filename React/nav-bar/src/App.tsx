f850f3a10778d149d920775dc0e4a18aa36c1d95
Asad Bukhari <asad.bukhari@bentley.com>
2022-08-24 10:06:33 +0500

    Nav bar post

--- React/nav-bar/src/App.tsx
+++ React/nav-bar/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import { Bookings } from "./components/booking/Bookings";
 import { ContactUs } from "./components/contact/ContactUs";
 import { Destinations } from "./components/destination/Destinations";
